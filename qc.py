e9519351da607eccf1dc446c59b322cb8cc440a7
araag2 <artur.guimas@gmail.com>
2020-10-24 16:06:47 +0100

    Final delivery, don't touch this

--- qc.py
+++ qc.py
@@ -12,23 +12,6 @@ import nltk
 #-----------------------------------------------------------------
 # Global Variable Field 
 #-----------------------------------------------------------------
-'''
-NLTK StopWordList
-['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", 
-"you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 
-'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 
-'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 
-'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 
-'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 
-'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 
-'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 
-'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 
-'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 
-"aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 
-'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 
-'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
-'''
-
 stop_words_coarse = ['i', 'me', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", 
 "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'his', 'she', "she's", 
 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'them', 'their', 'theirs', 'what', 
@@ -95,7 +78,6 @@ def preprocess_line(line):
     # Tokenization 
     p_line = nltk.word_tokenize(p_line)
 
-    # TODO: Check this
     # Stemmerization and Lemmatization
     p_line = stem_lem(p_line)
 
@@ -177,12 +159,6 @@ def read_train_data(file_name):
 
     train_vectorizer = TfidfVectorizer()
     vec_data = train_vectorizer.fit_transform(p_train_set)
-
-    # Best so far: Coarse [SVC(C=100.0, gamma=1)]
-    # 82.14080% Coarse [SVC(C=70.0, gamma=0.5)]
-    # Best so far: Fine [SVC(C=100.0, gamma=0.1)]
-    # 75.81% [SVC(C=20, gamma=0.4)]
-    # 78.023134% [SVC(C=100, gamma=0.2)]
 
     if coarse:
         svm_classifier = svm.SVC(kernel='rbf', C=100.0, gamma=1.0)
